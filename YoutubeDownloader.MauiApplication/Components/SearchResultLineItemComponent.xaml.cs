f37b99799c54197567953cf9f0ef656c77fdf12d
wasyster <wasyster@gmail.com>
2024-11-07 18:49:17 +0100

    select all from playlist refactor

--- YoutubeDownloader.MauiApplication/Components/SearchResultLineItemComponent.xaml.cs
+++ YoutubeDownloader.MauiApplication/Components/SearchResultLineItemComponent.xaml.cs
@@ -2,58 +2,18 @@ namespace YoutubeDownloader.MauiApplication.Components;
 
 public partial class SearchResultLineItemComponent : ContentView
 {
-    public static readonly BindableProperty DownloadProperty = BindableProperty.Create(
-        propertyName: nameof(Download),
-        returnType: typeof(bool),
+    public static readonly BindableProperty SearchResultProperty = BindableProperty.Create(
+        propertyName: nameof(SearchResult),
+        returnType: typeof(SearchResult),
         declaringType: typeof(SearchResultLineItemComponent),
         defaultValue: null,
         defaultBindingMode: BindingMode.TwoWay);
 
-	public static readonly BindableProperty TitleProperty = BindableProperty.Create(
-		propertyName: nameof(Title),
-		returnType: typeof(string),
-		declaringType: typeof(SearchResultLineItemComponent),
-		defaultValue: null,
-		defaultBindingMode: BindingMode.TwoWay);
-
-	public static readonly BindableProperty ThumbnailProperty = BindableProperty.Create(
-		propertyName: nameof(Thumbnail),
-		returnType: typeof(string),
-		declaringType: typeof(SearchResultLineItemComponent),
-		defaultValue: null,
-		defaultBindingMode: BindingMode.TwoWay);
-
-	public static readonly BindableProperty OnlyAudioProperty = BindableProperty.Create(
-		propertyName: nameof(OnlyAudio),
-		returnType: typeof(bool),
-		declaringType: typeof(SearchResultLineItemComponent),
-		defaultValue: null,
-		defaultBindingMode: BindingMode.TwoWay);
-
-	public bool Download
+	public SearchResult SearchResult
     {
-        get => (bool)GetValue(DownloadProperty);
-        set => SetValue(DownloadProperty, value);
+        get => (SearchResult)GetValue(SearchResultProperty);
+        set => SetValue(SearchResultProperty, value);
     }
-
-    public string Title
-    {
-		get => (string)GetValue(TitleProperty);
-		set => SetValue(TitleProperty, value);
-	}
-
-	public string Thumbnail
-	{
-		get => (string)GetValue(ThumbnailProperty);
-		set => SetValue(ThumbnailProperty, value);
-	}
-
-	public bool OnlyAudio
-	{
-		get => (bool)GetValue(OnlyAudioProperty);
-		set => SetValue(OnlyAudioProperty, value);
-	}
-
 
 	public SearchResultLineItemComponent()
 	{
