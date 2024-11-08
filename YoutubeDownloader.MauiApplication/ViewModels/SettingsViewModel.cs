7fac60f8e9840d09808a47605c240571ac312ac5
atila.vastag <atila.vastag@p92.hu>
2024-11-08 15:44:53 +0100

    settings page

--- YoutubeDownloader.MauiApplication/ViewModels/SettingsViewModel.cs
+++ YoutubeDownloader.MauiApplication/ViewModels/SettingsViewModel.cs
@@ -1,6 +1,31 @@
 ﻿namespace YoutubeDownloader.MauiApplication.ViewModels;
 
-public partial class SettingsViewModel : SettingsModel
+[ObservableObject]
+public partial class SettingsViewModel(IDbContextService<SettingsModel> dbSettingsContext)
 {
+    [ObservableProperty]
+    private SettingsModel settings;
 
+    public IAsyncRelayCommand AppearingCommand => new AsyncRelayCommand(OnAppearingkAsync);
+
+    public IAsyncRelayCommand ChangeDownloadFolderCommand => new AsyncRelayCommand(ChangeDownloadFolderAsync);
+
+    private async Task OnAppearingkAsync()
+    {
+        Settings = await dbSettingsContext.GetAsync(DatabeseKeys.Settings);
+    }
+
+    private async Task ChangeDownloadFolderAsync()
+    {
+        var result = await FolderPicker.Default.PickAsync();
+        if (!result.IsSuccessful)
+        {
+            await Toast.Make($"The folder was not picked").Show();
+            await Task.Delay(1000);
+        }
+        else
+        {
+            await dbSettingsContext.CreateOrUpdateIfExistsAsync(new SettingsModel(result));
+        }
+    }
 }
